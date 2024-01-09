e0196f466279d0f3d72cec4c54366271aabfee11
bayudsatriyo <bayudsatriyo@gmail.com>
2024-01-09 23:42:44 +0700

    fix all

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,24 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   NavMenu.classList.toggle("hidden");
 });
+
+// Mengambil semua elemen link
+const links = document.querySelectorAll("a");
+
+// Menambahkan event listener pada setiap link
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // Mengambil id dari elemen tujuan
+    const id = link.getAttribute("href");
+    // Jika id diawali dengan tanda pagar, maka link internal
+    if (id.startsWith("#")) {
+      // Mencegah perilaku default dari link
+      e.preventDefault();
+      // Mengambil elemen tujuan berdasarkan id
+      const target = document.querySelector(id);
+      // Menggerakkan elemen tujuan ke dalam pandangan dengan efek smooth
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+    // Jika tidak, maka link eksternal dan biarkan perilaku default
+  });
+});
